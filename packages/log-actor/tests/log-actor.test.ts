bbf21f9979e209e0e9a3390c85c1014a52d27fed
anton <anton.nagornyi@gmail.com>
2023-11-29 09:30:26 +0200

    fix: log-actor tests

--- packages/log-actor/tests/log-actor.test.ts
+++ packages/log-actor/tests/log-actor.test.ts
@@ -4,10 +4,11 @@ import { SetLogLevel } from '../src/messages/set-log-level';
 import { LogMessage } from '../src/messages/log-message';
 
 describe('Given a LogActor', () => {
-  let logActor: ActorRef<LogActor>;
+  let logActor: ActorRef;
   let mockConsoleLog: jest.SpyInstance;
   let mockConsoleWarn: jest.SpyInstance;
   let mockConsoleError: jest.SpyInstance;
+  const wait = () => new Promise((resolve) => setTimeout(resolve));
 
   beforeEach(() => {
     logActor = ActorSystem.create(LogActor, {
@@ -26,8 +27,9 @@ describe('Given a LogActor', () => {
   });
 
   describe('When receiving a SetLogLevel message', () => {
-    beforeEach(() => {
+    beforeEach(async () => {
       logActor.tell(new SetLogLevel({ logLevel: 'error' }));
+      await wait();
     });
 
     describe('And logging info message', () => {
@@ -39,49 +41,56 @@ describe('Given a LogActor', () => {
 
   describe('When logging a message', () => {
     describe('And log level is set to info', () => {
-      beforeEach(() => {
+      beforeEach(async () => {
         logActor.tell(new SetLogLevel({ logLevel: 'info' }));
+        await wait();
       });
 
-      it('Then it should not log silly message', () => {
+      it('Then it should not log silly message', async () => {
         logActor.tell(
           new LogMessage({ level: 'silly', message: 'Silly message' }),
         );
+        await wait();
         expect(mockConsoleLog).not.toBeCalled();
       });
 
-      it('Then it should not log debug message', () => {
+      it('Then it should not log debug message', async () => {
         logActor.tell(
           new LogMessage({ level: 'debug', message: 'Debug message' }),
         );
+        await wait();
         expect(mockConsoleLog).not.toBeCalled();
       });
 
-      it('Then it should log info message', () => {
+      it('Then it should log info message', async () => {
         logActor.tell(
           new LogMessage({ level: 'info', message: 'Info message' }),
         );
+        await wait();
         expect(mockConsoleLog).toBeCalledWith('Info message');
       });
 
-      it('Then it should log warn message', () => {
+      it('Then it should log warn message', async () => {
         logActor.tell(
           new LogMessage({ level: 'warn', message: 'Warn message' }),
         );
+        await wait();
         expect(mockConsoleWarn).toBeCalledWith('Warn message');
       });
 
-      it('Then it should log error message', () => {
+      it('Then it should log error message', async () => {
         logActor.tell(
           new LogMessage({ level: 'error', message: 'Error message' }),
         );
+        await wait();
         expect(mockConsoleError).toBeCalledWith('Error message');
       });
 
-      it('Then it should log fatal message', () => {
+      it('Then it should log fatal message', async () => {
         logActor.tell(
           new LogMessage({ level: 'fatal', message: 'Fatal message' }),
         );
+        await wait();
         expect(mockConsoleError).toBeCalledWith('Fatal message');
       });
     });
@@ -91,17 +100,19 @@ describe('Given a LogActor', () => {
     const testMessage = 'Test message';
     const testMeta = { data: 'Some meta data' };
 
-    it('Then it should handle only message', () => {
+    it('Then it should handle only message', async () => {
       logActor.tell(new LogMessage({ level: 'info', message: testMessage }));
+      await wait();
       expect(mockConsoleLog).toBeCalledWith(testMessage);
     });
 
-    it('Then it should handle only meta', () => {
+    it('Then it should handle only meta', async () => {
       logActor.tell(new LogMessage({ level: 'info', meta: [testMeta] }));
+      await wait();
       expect(mockConsoleLog).toBeCalledWith(testMeta);
     });
 
-    it('Then it should handle message and meta', () => {
+    it('Then it should handle message and meta', async () => {
       logActor.tell(
         new LogMessage({
           level: 'info',
@@ -109,6 +120,7 @@ describe('Given a LogActor', () => {
           meta: [testMeta],
         }),
       );
+      await wait();
       expect(mockConsoleLog).toBeCalledWith(testMessage, testMeta);
     });
   });
