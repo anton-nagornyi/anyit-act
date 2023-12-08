74b0f11bfbad55f528df3060f66c20a337c71b92
anton <anton.nagornyi@gmail.com>
2023-12-08 07:24:45 +0200

    feat: allow to set fallback environment in the manager

--- packages/actor/src/environment/environment-manager.ts
+++ packages/actor/src/environment/environment-manager.ts
@@ -2,13 +2,14 @@ import { Environment } from './environment';
 import { LocalEnvironment } from './local-environment';
 
 export class EnvironmentManager {
-  constructor(private readonly environments: Environment[] = []) {}
-
-  private localEnvironment = new LocalEnvironment();
+  constructor(
+    private readonly environments: Environment[] = [],
+    private readonly fallbackEnvironment = new LocalEnvironment(),
+  ) {}
 
   getEnvironmentByAddress(address?: string | null) {
     if (!address) {
-      return this.localEnvironment;
+      return this.fallbackEnvironment;
     }
 
     const [name] = address.split(':');
@@ -21,6 +22,6 @@ export class EnvironmentManager {
         return env;
       }
     }
-    return this.localEnvironment;
+    return this.fallbackEnvironment;
   }
 }
